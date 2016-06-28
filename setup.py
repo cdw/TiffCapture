ffdc89de91b3756ade993ab217e7aca3e3340824
Dave Williams <cdave@uw.edu>
2016-06-28 16:56:41 -0700

    Push to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='TiffCapture',
-        version='0.1.3',
+        version='0.1.4',
         author='Dave Williams',
         author_email='cdave@uw.edu',
         packages=['tiffcapture'],
